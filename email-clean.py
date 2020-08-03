3ccdc4c468ff4d0142975e74f92f681014014026
marco <crivaronicolini@hotmail.com>
2020-08-02 22:35:44 -0300

    parece que anda todo, quiero ver que esta pasanndo con buff, nunca lo agrego

--- email-clean.py
+++ email-clean.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from collections import defaultdict
 
 campos_listados = ["To", "X-To", "X-cc", "X-bcc"]
@@ -6,54 +7,75 @@ campos_listados = ["To", "X-To", "X-cc", "X-bcc"]
 fields = {"Message-ID": "",
           "Date": "",
           "From": "",
-          "To": [],
+          "To": "",
+          "Cc": "",
+          "Bcc": "",
           "Subject": "",
           "Mime-Version": "",
           "Content-Type": "",
           "Content-Transfer-Encoding": "",
           "X-From": "",
-          "X-To": [],
-          "X-cc": [],
-          "X-bcc": [],
+          "X-To": "",
+          "X-cc": "",
+          "X-bcc": "",
           "X-Folder": "",
-          "X-Origin": ""}
+          "X-Origin": "",
+          "user": ""}
 
 personas = os.listdir('./maildir')
-dataset = defaultdict(list)
+dataset = []
+# dataset = defaultdict(list)
 
 walk = open('walksort')
-# walker = os.walk('./maildir/' + persona)
-# breakpoint()
-# for dire, folders, mails in list(walker):
 for mail in walk.read().splitlines():
     # print(mails)
     lines = []
-    with open('./maildir/' + mail) as m:
-        for line in m.read().splitlines():
-            print(line)
-            if line.startswith('X-FileName:'):
-                break
-            lines.append(line)
+    # with open('./maildir/' + mail) as m:
+    try:
+        with open('./maildir/' + mail, encoding='cp1252') as m:
+            for line in m.read().splitlines():
+                # print(line)
+                if line.startswith('X-FileName:'):
+                    break
+                lines.append(line)
+    except UnicodeDecodeError:
+        print(mail)
+        continue
     data = fields.copy()
     buff = []
+    buffkey = ""
     for line in lines:
         particion = line.partition(': ')
         # si partition encuentra el separador, y lo que esta a la derecha del
         # sep no es nulo, lo agrego al campo, y sino agrego esa linea al buffer
         # si encuentro el separador en una linea que nada que ver, la agrego al
         # buffer y la imprimo
+
+        # data[particion[0]] = particion[2]
         if particion[2] == "":
             buff.append(particion[0])
+            # print(buff)
             # breakpoint()
         elif particion[0] in data.keys():
+            if buff:
+                data
             data[particion[0]] = particion[2]
             buff = []
         else:
+            # cosas que no son los keys pero se escapan lo mando al buff
             buff.append(line)
             print(line)
     # for campo in campos_listados:
     #     data[campo] = data[campo].split(',')
     persona = mail.split('/')[0]
-    dataset[persona].append(data)
-print(dataset)
+    data['user'] = persona
+    dataset.append(data)
 walk.close()
+df = pd.DataFrame(dataset)
+# df = dataset.drop(['Mime-Version', 'Content-Type',
+#                    'Content-Transfer-Encoding'], axis=1)
+# # Parse datetime
+# df['Date'] = pd.to_datetime(
+#     df['Date'], infer_datetime_format=True, errors='coerce')
+
+# df[df['Date'] == 'Mon, 12 Mar 2001 00:00:00']
